c7040ce8685cca788d833235a0593a3751e9613a
Elioenay <elionay407@gmail.com>
2023-08-21 14:08:09 -0300

    Manipulando cookies no Next.js

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -8,6 +8,7 @@ import { z } from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { api } from '@/lib/axios'
+import { AxiosError } from 'axios'
 
 const registerFormSchema = z.object({
   username: z
@@ -41,6 +42,10 @@ export default function Register() {
         username: data.username,
       })
     } catch (error) {
+      if (error instanceof AxiosError && error?.response?.data?.message) {
+        alert(error.response.data.message)
+        return
+      }
       console.log(error)
     }
   }
