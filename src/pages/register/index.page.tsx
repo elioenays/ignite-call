8e25fa99d18b9ca545d80bf26e4e4a8152c57eb4
Elioenay <elionay407@gmail.com>
2023-08-21 11:31:22 -0300

    Estrutura visual do cadastro

---
+++ src/pages/register/index.page.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { ArrowRight } from 'phosphor-react'
+
+import { Container, Form, Header } from './styles'
+import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+
+export default function Register() {
+  return (
+    <Container>
+      <Header>
+        <Heading as='strong'>Bem-vindo ao Ignite Call!</Heading>
+        <Text>
+          Precisamos de algumas informações para criar seu perfil! Ah, você pode
+          editar essas informações depois.
+        </Text>
+
+        <MultiStep
+          size={4}
+          currentStep={1}
+        />
+      </Header>
+      <Form as='form'>
+        <label>
+          <Text size='sm'>Nome de usuário</Text>
+          <TextInput
+            placeholder='seu-usuário'
+            prefix='ignite.com/'
+            crossOrigin=''
+          />
+        </label>
+        <label>
+          <Text size='sm'>Nome completo</Text>
+          <TextInput
+            placeholder='seu-usuário'
+            prefix='ignite.com/'
+            crossOrigin=''
+          />
+        </label>
+        <Button type='submit'>
+          Próximo passo <ArrowRight />
+        </Button>
+      </Form>
+    </Container>
+  )
+}
