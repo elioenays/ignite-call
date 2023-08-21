00e7bdd059d078adc215fde4fbeea6b363cd5a83
Elioenay <elionay407@gmail.com>
2023-08-21 11:17:10 -0300

    Validação com Zod

--- src/pages/Home/ClaimUsernameForm/styles.ts
+++ src/pages/Home/ClaimUsernameForm/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, Text, styled } from '@ignite-ui/react'
 
 export const Form = styled(Box, {
   display: 'grid',
@@ -8,4 +8,10 @@ export const Form = styled(Box, {
   padding: '$4',
 
   '@media(max-width: 600px)': { gridTemplateColumns: '1fr' },
+})
+
+export const FormAnnotation = styled('div', {
+  marginTop: '$2',
+
+  [`> ${Text}`]: { color: '$gray400' },
 })
