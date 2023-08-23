7d55f6d90d2c846f258b8b995d9aac33e196d038
Elioenay <elionay407@gmail.com>
2023-08-23 17:55:23 -0300

    Estrutura visual da lista de horários

--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 import Calendar from '@/components/Calendar'
 
 export default function CalendarStep() {
+  const isDateSelected = true
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            terça-feira <span>20 de setembro</span>
+          </TimePickerHeader>
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
