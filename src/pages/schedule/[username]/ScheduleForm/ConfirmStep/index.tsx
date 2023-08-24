aae5c33700daff04b375762784fe6e11e3fcb68a
Elioenay <elionay407@gmail.com>
2023-08-24 10:50:56 -0300

    Página de confirmação

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,6 +1,58 @@
 import React from 'react'
 import CalendarStep from '../CalendarStep'
+import { ConfirmForm, FormActions, FormHeader } from './styles'
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
 
 export default function ConfirmStep() {
-  return <div></div>
+  async function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm
+      as='form'
+      onSubmit={handleConfirmScheduling}
+    >
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de setembro de 2023
+        </Text>
+        <Text>
+          <Clock />
+          15:00h
+        </Text>
+      </FormHeader>
+      <label>
+        <Text size='sm'>Nome completo</Text>
+        <TextInput
+          placeholder='Seu nome'
+          crossOrigin=''
+        />
+      </label>
+
+      <label>
+        <Text size='sm'>Endereço de e-mail</Text>
+        <TextInput
+          placeholder='jhondoe@example.com'
+          type='email'
+          crossOrigin=''
+        />
+      </label>
+
+      <label>
+        <Text size='sm'>Observações</Text>
+        <TextArea />
+      </label>
+
+      <FormActions>
+        <Button
+          type='button'
+          variant='tertiary'
+        >
+          Cancelar
+        </Button>
+        <Button type='submit'>Confirmar</Button>
+      </FormActions>
+    </ConfirmForm>
+  )
 }
