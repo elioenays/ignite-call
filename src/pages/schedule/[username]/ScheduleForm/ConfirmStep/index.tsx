382ea19e967b3cec9cb6eb66c4e1a02bb34a8463
Elioenay <elionay407@gmail.com>
2023-08-24 10:35:12 -0300

    Página de confirmação

--- src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import CalendarStep from '../CalendarStep'
 
-export default function ScheduleForm() {
-  return <CalendarStep />
+export default function ConfirmStep() {
+  return <div></div>
 }
